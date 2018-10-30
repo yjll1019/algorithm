2a0ac670980d99f61239c712fa588853725444c3
YejiLee <32984517+yjll1019@users.noreply.github.com>
2018-10-30 21:34:26 +0900

    분할 정복 - 버블 소트 몰랐던 부분 해결
    분할 정복 - 버블 소트 몰랐던 부분 해결

--- Baekjoon/divideConquer/Code_1517.java
+++ Baekjoon/divideConquer/Code_1517.java
@@ -40,24 +40,15 @@ public class Code_1517 {
 		int[] brr = new int[e-s+1];
 		
 		
-		while(a<=m || b<=e) {
-			if (a <= m && (b > e || arr[a] <= arr[b])) {
+		while(a<=m || b<=e) { //원소가 하나가 남을 때까지 while문이 반복되어야하기 때문에 or연산자 
+			if (a <= m && (b > e || arr[a] <= arr[b])) { //오른쪽 영역이 다 정렬된 후 왼쪽 정렬도 배열에 넣어주기 위해서 b<e 추가
 				brr[i++] = arr[a++];
 			}else {
 				ans+=(long) m-a+1;
 				brr[i++] = arr[b++];
 			}
 		}
-		/*틀림
-		while(a<=m && b<=e) {
-			if(arr[a]<=arr[b]) {
-				brr[i++] = arr[a++];
-			}else {
-				ans+=(long) m-a+1;
-				brr[i++] = arr[b++];
-			}
-		}
-		*/
+
 		for(int j=s; j<=e; ++j) {
 			arr[j] = brr[j-s];
 		}
