de30a6c680b4799bef33af2c2f8814b5d99364eb
HYE_JI <lwiiiiii@naver.com>
2019-02-24 20:06:13 +0900

    카카오톡 로그인 토큰 받아오기

--- mandalart_server/src/main/java/com/mandalart_server/controller/KAuthController.java
+++ mandalart_server/src/main/java/com/mandalart_server/controller/KAuthController.java
@@ -1,11 +1,51 @@
 package com.mandalart_server.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.message.BasicNameValuePair;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("auth")
 public class KAuthController {
 
+    @RequestMapping
+    public void login(@RequestParam("code") String code) {
+        String requestURL = "https://kauth.kakao.com/oauth/token";
+        String redirect_uri = "http://localhost:8080/auth";
+
+        List<NameValuePair> nameValuePairs = new ArrayList<>();
+        nameValuePairs.add(new BasicNameValuePair("grant_type","authorization_code" ));
+        nameValuePairs.add(new BasicNameValuePair("client_id","" ));
+        nameValuePairs.add(new BasicNameValuePair("redirect_uri",redirect_uri ));
+        nameValuePairs.add(new BasicNameValuePair("code", code ));
+
+        CloseableHttpClient client = HttpClientBuilder.create().build();
+        HttpPost httpPost = new HttpPost(requestURL);
+
+        JsonNode jsonNode = null;
+
+        try {
+            httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+            HttpResponse response = client.execute(httpPost);
+
+            jsonNode = new ObjectMapper().readTree(response.getEntity().getContent());
+
+            System.out.println("토큰: " + jsonNode.get("access_token").toString());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 }
