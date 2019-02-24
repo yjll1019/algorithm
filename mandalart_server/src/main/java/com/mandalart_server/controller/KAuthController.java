53dfdd6822ed0c4a3bac69c230e0895002797214
HYE_JI <lwiiiiii@naver.com>
2019-02-24 21:27:49 +0900

    카카오톡 사용자 정보 받아오기

--- mandalart_server/src/main/java/com/mandalart_server/controller/KAuthController.java
+++ mandalart_server/src/main/java/com/mandalart_server/controller/KAuthController.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
@@ -26,7 +27,7 @@ public class KAuthController {
 
         List<NameValuePair> nameValuePairs = new ArrayList<>();
         nameValuePairs.add(new BasicNameValuePair("grant_type","authorization_code" ));
-        nameValuePairs.add(new BasicNameValuePair("client_id","" ));
+        nameValuePairs.add(new BasicNameValuePair("client_id","4cceb8e624030f4a73d760f1406752d5" ));
         nameValuePairs.add(new BasicNameValuePair("redirect_uri",redirect_uri ));
         nameValuePairs.add(new BasicNameValuePair("code", code ));
 
@@ -34,16 +35,35 @@ public class KAuthController {
         HttpPost httpPost = new HttpPost(requestURL);
 
         JsonNode jsonNode = null;
+        String access_token = "";
 
         try {
+
             httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
             HttpResponse response = client.execute(httpPost);
 
             jsonNode = new ObjectMapper().readTree(response.getEntity().getContent());
-
-            System.out.println("토큰: " + jsonNode.get("access_token").toString());
+            access_token = jsonNode.get("access_token").toString();
 
         } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        requestURL = "https://kapi.kakao.com/v2/user/me";
+
+        HttpGet httpGet = new HttpGet(requestURL);
+
+        try{
+
+            httpGet.setHeader("Authorization", "Bearer " + access_token);
+            HttpResponse response = client.execute(httpGet);
+
+            jsonNode = new ObjectMapper().readTree(response.getEntity().getContent());
+            System.out.println("id: " + jsonNode.get("id"));
+            System.out.println("nickname: " + jsonNode.get("properties").get("nickname"));
+            System.out.println("email: " + jsonNode.get("kakao_account").get("email"));
+
+        }catch (Exception e){
             e.printStackTrace();
         }
     }
