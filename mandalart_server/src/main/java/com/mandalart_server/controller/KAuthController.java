6c810c93423cb3e7f852bedbd80dab1c6d9e39b9
HYE_JI <lwiiiiii@naver.com>
2019-03-03 14:24:23 +0900

    Service 분리, KAuthController login -> getUserInfo

--- mandalart_server/src/main/java/com/mandalart_server/controller/KAuthController.java
+++ mandalart_server/src/main/java/com/mandalart_server/controller/KAuthController.java
@@ -1,71 +1,25 @@
 package com.mandalart_server.controller;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.message.BasicNameValuePair;
+import com.mandalart_server.service.KAuthService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 @RequestMapping("auth")
 public class KAuthController {
 
+    @Autowired
+    KAuthService kAuthService;
+
     @RequestMapping
-    public void login(@RequestParam("code") String code) {
-        String requestURL = "https://kauth.kakao.com/oauth/token";
-        String redirect_uri = "http://localhost:8080/auth";
+    public void getUserInfo(@RequestParam("code") String code) {
 
-        List<NameValuePair> nameValuePairs = new ArrayList<>();
-        nameValuePairs.add(new BasicNameValuePair("grant_type","authorization_code" ));
-        nameValuePairs.add(new BasicNameValuePair("client_id","4cceb8e624030f4a73d760f1406752d5" ));
-        nameValuePairs.add(new BasicNameValuePair("redirect_uri",redirect_uri ));
-        nameValuePairs.add(new BasicNameValuePair("code", code ));
-
-        CloseableHttpClient client = HttpClientBuilder.create().build();
-        HttpPost httpPost = new HttpPost(requestURL);
-
-        JsonNode jsonNode = null;
-        String access_token = "";
-
-        try {
-
-            httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-            HttpResponse response = client.execute(httpPost);
-
-            jsonNode = new ObjectMapper().readTree(response.getEntity().getContent());
-            access_token = jsonNode.get("access_token").toString();
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        requestURL = "https://kapi.kakao.com/v2/user/me";
-
-        HttpGet httpGet = new HttpGet(requestURL);
-
-        try{
-
-            httpGet.setHeader("Authorization", "Bearer " + access_token);
-            HttpResponse response = client.execute(httpGet);
-
-            jsonNode = new ObjectMapper().readTree(response.getEntity().getContent());
-            System.out.println("id: " + jsonNode.get("id"));
-            System.out.println("nickname: " + jsonNode.get("properties").get("nickname"));
-            System.out.println("email: " + jsonNode.get("kakao_account").get("email"));
-
-        }catch (Exception e){
-            e.printStackTrace();
-        }
+        kAuthService.getUserInfo(kAuthService.accessToken(code));
+        /* 출력 예시
+        *  id: 123456789
+        *  nickname: 홍길동
+        *  email: xxxxxxx@xxxx.com
+        * */
     }
 
 }
